591124127bd018f31063a3e96fcfd2d32a95af8c
PanagiotisEvagelidakis <pevagelidakis@athtech.gr>
2023-10-01 15:13:53 +0300

    connect question service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,7 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { QuestionComponent } from './question/question.component';
 import { HeaderComponent } from './header/header.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button'
-import {MatInputModule} from '@angular/material/input'
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -23,7 +21,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { AppComponent } from '../app.component';
 import { WelcomeComponent } from '../welcome/welcome.component';
+import { QuestionService } from '../service/question.service';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.scss']
 })
-export class QuestionComponent {
+export class QuestionComponent implements OnInit{
   title = new AppComponent().title
   name = new WelcomeComponent().data.value.username
+
+  constructor(private questionService: QuestionService){}
+
+  ngOnInit(): void {
+    this.getAllQuestions()
+  }
+  getAllQuestions(){
+    this.questionService.getQuestionJson().subscribe(res =>{
+      console.log(res);
+    })
+  }
+
 }
